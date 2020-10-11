ca248f513a0dbff454ab1c10b1c3946ce47ffc0b
sukirark <sukira_rk@yahoo.com>
2020-10-11 11:38:31 -0400

    Add project files.

---
+++ RK_TestApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RK_TestApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
